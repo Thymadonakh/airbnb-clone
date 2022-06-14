3f165b68080b87d31c2bdc341da2f28253d699d1
Thy Madona <thymadonakh@gmail.com>
2022-06-14 13:15:24 +0700

    airbnb clone react

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function NavBar() {
+    return (
+        <nav>
+            <img src='./images/airbnb-logo.png' alt="logo" className="nav--logo"/>
+        </nav>
+    );
+}
+
+
